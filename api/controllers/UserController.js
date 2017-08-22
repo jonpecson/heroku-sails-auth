2042cb752abdfac7c162087e0ff7906472ced660
John <jonpecson.io@gmail.com>
2017-08-22 18:30:59 +0800

    Email Support

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -18,6 +18,13 @@
 
 var _ = require('lodash');
 
+// Mailgun
+var api_key = 'key-59e03ff1ee4cc411b045ce02aff4c71a';
+var domain = 'mg.cooptavanza.net';
+var mailgun = require('mailgun-js')({ apiKey: api_key, domain: domain });
+
+
+
 module.exports = {
     create: function(req, res) {
         if (req.body.password !== req.body.confirmPassword) {
@@ -36,6 +43,37 @@ module.exports = {
                 token: JwtService.issue({ id: user.id })
             }
             var url = "http://localhost:1337/accounts/activate" + "?id=" + user.id + "&token=" + JwtService.issue({ id: user.id });
+
+
+            var message = '';
+            message = 'Hello!';
+            message += '<br/>';
+            message += 'Please visit the verification link to complete the registration process.';
+            message += '<br/><br/>';
+            message += '<a href="';
+            message += url;
+            message += '">Verification Link</a>';
+            message += '<br/><br/>';
+            message = 'Regards,';
+            message += '<br/>';
+            message += 'Cooptavanza R.L Team';
+
+
+            var data = {
+                from: 'Cooptavanza<support@cooptavanza.net>',
+                to: user.email,
+                subject: 'Please verify your email',
+                html: message
+            };
+
+            mailgun.messages().send(data, function(error, body) {
+                console.log("Email Response:", body);
+                console.log(message)
+            });
+
+
+
+
             return ResponseService.json(200, res, "User created successfully", responseData, url)
         }).catch(function(error) {
             if (error.invalidAttributes) {
